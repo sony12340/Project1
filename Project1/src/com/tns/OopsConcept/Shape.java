3904b2d6ca3611bf54bf7bd79fba865dc04bc495
patil <patil@VIJAYLAXMI>
2024-05-04 13:52:47 +0530

    second commit

---
+++ Project1/src/com/tns/OopsConcept/Shape.java
@@ -0,0 +1,17 @@
+// program to demonstrate classes and objects
+
+package com.tns.OopsConcept;
+
+public class Shape {
+	int length=2;// data members
+	int bredth=3;
+	int ans;
+	
+	void area() // member function
+	{
+		ans=length*bredth;
+		System.out.println("the area is"+ans);
+	}
+	
+
+}
---
+++ Project1/src/com/tns/OopsConcept/ShapeDemo.java
@@ -0,0 +1,13 @@
+package com.tns.OopsConcept;
+
+public class ShapeDemo {
+	
+	public static void main(String[] args) {
+		//TODO Auto-generated method stub
+		
+		Shape ob=new Shape();// object creation
+		ob.area();
+		
+	}
+
+}
