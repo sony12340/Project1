8c256ad6cf382934dac3ab872bfcacc504a0ba2c
patil <patil@VIJAYLAXMI>
2024-05-04 13:48:37 +0530

    Second commit

---
+++ Project1/src/com/tns/encapsulation/Student.java
@@ -0,0 +1,27 @@
+package com.tns.encapsulation;
+
+public class Student {
+	private int rollno; // data member
+	private String name;
+	
+	public int getRollno() {
+		return rollno;
+	}
+	public void setRollno(int rollno) {
+		this.rollno = rollno;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "Student [rollno=" + rollno + ", name=" + name + "]";
+	}
+	
+	
+	
+
+}
